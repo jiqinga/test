d9cf0a7729998ec6f3905302dd7a5302777f57ee
woshijiqinga <1758812645@qq.com>
2019-04-26 03:11:10 -0400

    add install.sh

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+yum -y install ansible
+mv /etc/ansible/ansible.cfg  /etc/ansible/ansible.bak
+mv /etc/ansible/hosts     /etc/ansible/host.bak
+yum -y install git
+git clone https://github.com/woshijiqinga/ansible.git /etc/anisble/roles/
+mv /etc/anisble/roles/geerlingguy.kubernetes/defaults/k8s.yaml  /etc/ansible/
+mv /etc/anisble/roles/geerlingguy.kubernetes/defaults/node.yaml  /etc/ansible/
+mv /etc/anisble/roles/geerlingguy.kubernetes/defaults/hosts    /etc/ansible/
+yum -y install wget
+cd /etc/ansible 
+wget https://raw.githubusercontent.com/woshijiqinga/test/master/ansible.cfg
+
+
