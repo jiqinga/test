5fbea48628904c1380c3257ca2b31336cd95d1a7
jiqinga <41818729+jiqinga@users.noreply.github.com>
2019-12-03 11:36:58 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,11 +3,11 @@ yum -y install ansible
 mv /etc/ansible/ansible.cfg  /etc/ansible/ansible.bak
 mv /etc/ansible/hosts     /etc/ansible/host.bak
 yum -y install git
-git clone https://github.com/woshijiqinga/ansible.git /etc/ansible/roles/
+git clone https://github.com/jiqinga/ansible.git /etc/ansible/roles/
 cp -aP /etc/ansible/roles/geerlingguy.kubernetes/defaults/k8s.yaml  /etc/ansible/
 cp -aP /etc/ansible/roles/geerlingguy.kubernetes/defaults/node.yaml  /etc/ansible/
 cp -aP /etc/ansible/roles/geerlingguy.kubernetes/defaults/hosts    /etc/ansible/
 yum -y install wget
-cd /etc/ansible/ && wget https://raw.githubusercontent.com/woshijiqinga/test/master/ansible.cfg
+cd /etc/ansible/ && wget https://raw.githubusercontent.com/jiqinga/test/master/ansible.cfg
 
 
